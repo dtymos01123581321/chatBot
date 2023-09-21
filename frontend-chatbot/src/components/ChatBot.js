3445635dac381cb8ccb2843acf36f82ec6250ebd
Dmytro Tymoshyk <dtymoshyk@360insights.com>
2023-09-21 01:36:00 -0400

    updated chatbot ui

--- frontend-chatbot/src/components/ChatBot.js
+++ frontend-chatbot/src/components/ChatBot.js
@@ -6,6 +6,7 @@ const ChatBot = () => {
     const [messages, setMessages] = useState([]);
     const [response, setResponse] = useState([]);
     const [inputValue, setInputValue] = useState('');
+    const [isSelect, setIsSelect] = useState(false);
     const chatRef = useRef(null);
 
     const scrollToBottom = () => {
@@ -16,6 +17,12 @@ const ChatBot = () => {
 
     const handleInputChange = (e) => {
         setInputValue(e.target.value);
+    };
+
+    const handleSelectAnswerChange = (value) => {
+        console.log('value  --: ', value);
+        setIsSelect(false);
+        handleSend(value)
     };
 
     const fetchData = async (msg, step) => {
@@ -39,12 +46,15 @@ const ChatBot = () => {
 
     useEffect(() => {
         scrollToBottom();
-    }, [messages]);
+    }, [messages, isSelect]);
 
     useEffect(() => {
         if (response.length && response[0].indexOf('next') !== -1) {
             const newStep = response[0].split('-')[1]
             sendMsg('', newStep);
+        }
+        if (response.length && response[0].indexOf('yes') !== -1) {
+            setIsSelect(true);
         }
     }, [response]);
 
@@ -61,19 +71,20 @@ const ChatBot = () => {
         sendMsg('', 0);
     }, []);
 
-    const handleSend = () => {
-        if (inputValue.trim()) {
-            setMessages([...messages, { text: inputValue, sender: 'user' }]);
+    const handleSend = (selectValue) => {
+        if (inputValue.trim() || selectValue.trim()) {
+            const value = inputValue || selectValue;
+            setMessages([...messages, { text: value, sender: 'user' }]);
             setInputValue('');
 
-            const item = response.find(f => f.includes(inputValue));
+            const item = response.find(f => f.includes(value));
             const itemNoEmpty = response.find(f => f.includes('noEmpty'));
             const action = item || itemNoEmpty;
             if (action) {
                 const newStep = action.split('-')[1];
                 setTimeout(() => {
-                    fetchData(inputValue, newStep).then(data => {
-                        setMessages([...messages, { text: inputValue, sender: 'user' }, { text: data.msg, sender: 'bot' }]);
+                    fetchData(value, newStep).then(data => {
+                        setMessages([...messages, { text: value, sender: 'user' }, { text: data.msg, sender: 'bot' }]);
                         setResponse(data.nextSteps);
                     })
                 }, 1000);
@@ -85,14 +96,23 @@ const ChatBot = () => {
         <div className="chatbot">
             <div className="chat-history" ref={chatRef}>
                 {messages.length && messages.map((msg, index) => (
-                    <div key={index} className={`chat-message ${msg.sender}`}>
+                    <div key={index}
+                         className={`chat-message ${msg.sender}`}
+                         /*style={{ width: `${msg.text.length + 20}%`, maxWidth: '50%'}}*/>
                         <div className="chat-content">{msg.text}</div>
                     </div>
                 ))}
+                {isSelect && ['yes', 'no'].map((m, index) =>
+                    <div key={m + index}
+                         className="chat-message select-answer"
+                         /*style={{ width: `${m.length + 12}%`, maxWidth: '50%'}}*/
+                         onClick={() => handleSelectAnswerChange(m)}>
+                        <div className="chat-content">{m}</div>
+                </div>)}
             </div>
             <div className="chat-input">
-                <input value={inputValue} onChange={handleInputChange} onKeyDown={handleKeyPress}  placeholder="Type your message here..." />
-                <button onClick={handleSend} className="chat-send" />
+                <input value={inputValue} onChange={handleInputChange} onKeyDown={handleKeyPress}  placeholder="Type your message here..." disabled={isSelect}/>
+                <button onClick={handleSend} className="chat-send" disabled={isSelect} />
             </div>
         </div>
     );
